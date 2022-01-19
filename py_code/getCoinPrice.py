4b03e350588b4f38d033518767dfef1818a0892d
Mario Saucedo <mario_rsa@hotmail.com>
2022-01-19 07:39:19 -0600

    get coin price py

---
+++ py_code/getCoinPrice.py
@@ -0,0 +1,19 @@
+# simple script to fetch cryptocurrency prices
+
+import requests
+
+# add your iexapi key here
+api_key = 'YOUR API HERE'
+
+# pass in coin's symbol. list of symbols below
+def get_coin_price(symbol):
+    api_url = f'https://cloud.iexapis.com/stable/crypto/{symbol}/price?token={api_key}'
+    raw = requests.get(api_url).json()
+    price = raw['price']
+    return float(price)
+
+# fetch btc price
+btc = get_coin_price('btcusd')
+print("Price of bitcoin: {} USD".format(btc))
+
+# add here any other coin want to get the price
